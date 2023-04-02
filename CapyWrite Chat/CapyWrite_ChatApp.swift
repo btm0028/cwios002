eb4876bd87f2a98464a4388bf62ee4084e78bda3
btm0028 <btm0028@auburn.edu>
2023-04-01 20:37:46 -0400

    Initial Commit

---
+++ CapyWrite Chat/CapyWrite_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapyWrite_ChatApp.swift
+//  CapyWrite Chat
+//
+//  Created by Brandon  Miller on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CapyWrite_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
